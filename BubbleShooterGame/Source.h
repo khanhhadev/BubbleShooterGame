990ac7b7feadc165e9a75cf99b8305abee47de61
tuanndnd1 <107822113+tuanndnd1@users.noreply.github.com>
2022-07-16 13:36:53 +0700

    Update Source.h
    tuan commit ham delay cho createNewBubbleRow

--- BubbleShooterGame/Source.h
+++ BubbleShooterGame/Source.h
@@ -8,3 +8,31 @@ void createBubbleRow()
 	for (int i = 0; i < SCREEN_LENGTH; i++)
 		Bubble();
 }
+//ham delay thoi gian ma` minh muon
+void delay(int a)
+{
+	int add;
+	int time;
+	int i;
+
+// dieu chinh thoi gian delay o ben duoi
+	time = a*1000000000;
+	for (i = 0; i < time; i++)
+	{
+		add*=i;
+		add++;
+		add++;
+	}
+}
+int main()
+{
+while (true)
+	{
+		for(int i = 0; i < n; i++)
+			{
+				createNewBubbleRow();
+				delay(5);
+			}
+	}
+}
+
